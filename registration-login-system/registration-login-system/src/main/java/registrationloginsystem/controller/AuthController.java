543dab5c4a12e6ace4bf9d05dc6faf0e558f8769
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2024-03-01 11:00:12 -0300

    Criar Home Page

---
+++ registration-login-system/registration-login-system/src/main/java/registrationloginsystem/controller/AuthController.java
@@ -0,0 +1,11 @@
+package registrationloginsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class AuthController {
+    @GetMapping("/index")
+    public String home() {
+        return "index";
+    }
+}
