0361576fa715150d21592451c9fcc43b5951c4d4
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2024-03-01 22:39:53 -0300

    Registrar Usuarios

--- registration-login-system/registration-login-system/src/main/java/registrationloginsystem/controller/AuthController.java
+++ registration-login-system/registration-login-system/src/main/java/registrationloginsystem/controller/AuthController.java
@@ -1,11 +1,20 @@
 package registrationloginsystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import registrationloginsystem.dto.UserDto;
 @Controller
 public class AuthController {
     @GetMapping("/index")
     public String home() {
         return "index";
+    }
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+        return "register";
     }
 }
---
+++ registration-login-system/registration-login-system/src/main/java/registrationloginsystem/dto/UserDto.java
@@ -0,0 +1,16 @@
+package registrationloginsystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Long Id;
+}
